76d15bdfeac17e14e654c42862c9642c82f21af7
Your Name <you@example.com>
2024-08-07 21:40:31 +0300

    poslugi3

--- src/app/components/home/HomePage.jsx
+++ src/app/components/home/HomePage.jsx
@@ -1,41 +1,59 @@
-'use client';
+"use client";
 
 // import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
-import { useEffect, useState } from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import SwiperCore, { Autoplay } from 'swiper';
-import 'swiper/css';
-import styles from './styles.module.scss';
-import YouTube from 'react-youtube';
-import Link from 'next/link';
-import Carusel from '../rekl/carusel';
-import Footer from '../footer/Footer';
-import { redirect } from 'next/navigation';
-import axios from 'axios';
+import "react-quill/dist/quill.snow.css";
+import { useEffect, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { Autoplay } from "swiper";
+import "swiper/css";
+import styles from "./styles.module.scss";
+import YouTube from "react-youtube";
+import Link from "next/link";
+import Carusel from "../rekl/carusel";
+import Footer from "../footer/Footer";
+import { redirect } from "next/navigation";
+import axios from "axios";
 
 const HomePage = ({ photos }) => {
   SwiperCore.use([Autoplay]);
 
-  const TOKEN = '5530765545:AAFy5U47-r8OYc198-5blcgCR-cKB3_jowE';
-  const CHAT_ID = '-1001517912943';
+  const TOKEN = "5530765545:AAFy5U47-r8OYc198-5blcgCR-cKB3_jowE";
+  const CHAT_ID = "-1001517912943";
   const URI = `https://api.telegram.org/bot${TOKEN}/sendMessage`;
 
   const [actual, setActual] = useState(0);
 
-  const [name, setName] = useState('');
-  const [phone, setPhone] = useState('');
-  const [errorMsg, setErrorMsg] = useState('');
-  const [success, setSuccess] = useState('');
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [errorMsg, setErrorMsg] = useState("");
+  const [success, setSuccess] = useState("");
   const [data, setData] = useState({});
 
   const [about, setAbout] = useState([]);
+  const [serv, setServ] = useState([]);
+  const [buhserv, setBuhserv] = useState([]);
+  const [yurserv, setYurserv] = useState([]);
+  const [blockserv, setBlockserv] = useState([]);
+  const [finserv, setFinserv] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const res = await axios.get('/api/mainpage');
+        const res = await axios.get("/api/mainpage");
+        const res1 = await axios.get("/api/servicepage");
         setData(res.data);
+        setServ(res1.data);
+        const newBuh = res1.data.services.filter((s) => s.category == "buh");
+        setBuhserv(newBuh);
+        const newYur = res1.data.services.filter((s) => s.category == "yur");
+        setYurserv(newYur);
+        const newBlock = res1.data.services.filter(
+          (s) => s.category == "block"
+        );
+        setBlockserv(newBlock);
+        const newFin = res1.data.services.filter((s) => s.category == "fin");
+        setFinserv(newFin);
+        // console.log(newFin);
       } catch (error) {
         console.log(error);
       }
@@ -56,12 +74,12 @@ const HomePage = ({ photos }) => {
   // }, []);
 
   const sendMsg = async () => {
-    if (name === '') {
+    if (name === "") {
       setErrorMsg("Введіть будь-ласка своє Ім'я");
       return;
     }
-    if (phone === '') {
-      setErrorMsg('Введіть будь-ласка свій номер телефону');
+    if (phone === "") {
+      setErrorMsg("Введіть будь-ласка свій номер телефону");
       return;
     }
     const msg = `Запит на консультацію\n${name}\n${phone}`;
@@ -69,15 +87,15 @@ const HomePage = ({ photos }) => {
     const data = {
       name,
       phone,
-      mail: 'korotka@zsaytu.com',
-      category: 'Коротка',
-      item: 'Коротка',
-      comment: '0',
+      mail: "korotka@zsaytu.com",
+      category: "Коротка",
+      item: "Коротка",
+      comment: "0",
       price: 0,
     };
 
     try {
-      const res = await axios.post('/api/orders', data);
+      const res = await axios.post("/api/orders", data);
       console.log(res.data);
     } catch (error) {
       console.log(error);
@@ -87,17 +105,17 @@ const HomePage = ({ photos }) => {
       .post(URI, {
         chat_id: CHAT_ID,
         text: msg,
-        parse_mode: 'html',
+        parse_mode: "html",
         name,
         phone,
       })
       .then((res) => {
-        setSuccess('Вашу заявку прийнято');
-        setName('');
-        setPhone('');
+        setSuccess("Вашу заявку прийнято");
+        setName("");
+        setPhone("");
       })
       .catch((err) => {
-        console.log('bad');
+        console.log("bad");
       });
   };
 
@@ -112,27 +130,27 @@ const HomePage = ({ photos }) => {
     player.stopVideo();
   };
   const onError = (error) => {
-    console.error('YouTube Player Error:', error);
+    console.error("YouTube Player Error:", error);
   };
   return (
     <div className={styles.home}>
       {errorMsg && (
-        <div className={styles.alertWrapper} onClick={() => setErrorMsg('')}>
+        <div className={styles.alertWrapper} onClick={() => setErrorMsg("")}>
           <div className={styles.alert}>
             <p>{errorMsg}</p>
-            <button onClick={() => setErrorMsg('')}>Гаразд</button>
+            <button onClick={() => setErrorMsg("")}>Гаразд</button>
           </div>
         </div>
       )}
       {success && (
-        <div className={styles.alertWrapper} onClick={() => setSuccess('')}>
+        <div className={styles.alertWrapper} onClick={() => setSuccess("")}>
           <div className={styles.success}>
             <p>{success}</p>
-            <button onClick={() => setSuccess('')}>Гаразд</button>
+            <button onClick={() => setSuccess("")}>Гаразд</button>
           </div>
         </div>
       )}
-      <div className='container'>
+      <div className="container">
         <div className={styles.hero}>
           <div className={styles.heroLeft}>
             <h1 className={styles.heroTitle}>{data.title}</h1>
@@ -144,13 +162,13 @@ const HomePage = ({ photos }) => {
             {/* <p className={styles.heroText}>
               широкий спектр фінансових та бухгалтерських послуг для бізнесу
             </p> */}
-            <p className='text'>{data.slog}</p>
-            <Link href='/services' className={styles.button}>
+            <p className="text">{data.slog}</p>
+            <Link href="/services" className={styles.button}>
               Замовити послугу
             </Link>
           </div>
           <div className={styles.heroImg}>
-            <img src='/img/headerPhoto.webp' alt='' />
+            <img src="/img/headerPhoto.webp" alt="" />
           </div>
         </div>
         <div className={styles.youtube}>
@@ -162,8 +180,8 @@ const HomePage = ({ photos }) => {
             />
           </div>
           <div className={styles.yuText}>
-            <div className='redText'>про компанію</div>
-            <div className='title'>Наші сильні сторони</div>
+            <div className="redText">про компанію</div>
+            <div className="title">Наші сильні сторони</div>
             {/* <div className={'text' + ' ' + 'mt20'}>
               Компанія “Бухгалтер-консультант” працює в сферах фінансових та
               бухгалтерських послуг з 2020 року та надає повний спектр послуг
@@ -178,22 +196,22 @@ const HomePage = ({ photos }) => {
               будь-якої проблеми.
             </div> */}
             <div
-              className={'text' + ' ' + 'mt20'}
+              className={"text" + " " + "mt20"}
               dangerouslySetInnerHTML={{ __html: data.aboutText }}
             />
           </div>
         </div>
-        <div className={styles.poroposition + ' ' + 'mt100'}>
+        <div className={styles.poroposition + " " + "mt100"}>
           <div className={styles.propText}>
-            <div className='redText'>що ми пропонуємо</div>
-            <div className='title mb20'>Переваги співпраці з нами</div>
+            <div className="redText">що ми пропонуємо</div>
+            <div className="title mb20">Переваги співпраці з нами</div>
             <ul>
               {data.propositionItems &&
                 data.propositionItems.map((prop, idx) => {
                   return (
                     <li key={idx}>
-                      <img src='/img/galochka.svg' alt='' />
-                      <span className='text'>{prop}</span>
+                      <img src="/img/galochka.svg" alt="" />
+                      <span className="text">{prop}</span>
                     </li>
                   );
                 })}
@@ -243,16 +261,16 @@ const HomePage = ({ photos }) => {
               autoplay
               spaceBetween={20}
               slidesPerView={2}
-              onSlideChange={() => console.log('slide change')}
+              onSlideChange={() => console.log("slide change")}
               onSwiper={(swiper) => console.log(swiper)}
               modules={[Autoplay]}
             >
               {photos.workers.map((ab, idx) => {
-                console.log('qqq');
+                console.log("qqq");
                 if (!ab.unvisible) {
                   return (
                     <SwiperSlide key={idx}>
-                      <img src={ab.photo} alt='' />
+                      <img src={ab.photo} alt="" />
                     </SwiperSlide>
                   );
                 }
@@ -343,8 +361,8 @@ const HomePage = ({ photos }) => {
           </div> */}
         </div>
       </div>
-      <div className={styles.fishki + ' ' + 'mt100'}>
-        <div className={'container' + ' ' + styles.fishWrapper}>
+      <div className={styles.fishki + " " + "mt100"}>
+        <div className={"container" + " " + styles.fishWrapper}>
           <div className={styles.fishka}>
             <div className={styles.fishHeader}>4+</div>
             <div className={styles.fishBody}>років на ринку</div>
@@ -360,144 +378,106 @@ const HomePage = ({ photos }) => {
         </div>
       </div>
       <div className={styles.services}>
-        <div className='container'>
-          <div className='title mt100 center'>Наші послуги</div>
+        <div className="container">
+          <div className="title mt100 center">Бухгалтерські послуги</div>
           <div className={styles.serviceWrapper}>
-            <Link href='/services#consaltId'>
-              <div className={styles.service}>
-                <div className={styles.serHeader}>Консультації</div>
-                <div className={styles.serBody}>
-                  Бухгалтерський облік ФОП, ТОВ, фінанси бізнесу, гранти,
-                  інвестиції...
+            {buhserv?.map((buh, idx) => (
+              <Link key={idx} href={`/services#${buh.slogId}`}>
+                <div className={styles.service}>
+                  <div className={styles.serHeader}>{buh.name}</div>
+                  <div className={styles.serBody}>{buh.description}</div>
+                  <div className={styles.serLink}>Читати більше &#8594;</div>
                 </div>
-                <div className={styles.serLink}>Читати більше &#8594;</div>
-              </div>
-            </Link>
-            <Link href='/services#fopId'>
-              <div className={styles.service}>
-                <div className={styles.serHeader}>ФОП</div>
-                <div className={styles.serBody}>
-                  Реєстрація, супровід ФОП, наймані працівники, закриття ФОП...
-                </div>
-                <div className={styles.serLink}>Читати більше &#8594;</div>
-              </div>
-            </Link>
-            <Link href='/services#tovId'>
-              <div className={styles.service}>
-                <div className={styles.serHeader}>ТОВ</div>
-                <div className={styles.serBody}>
-                  Бухгалтерський супровід бізнесу в облікових програмах...
-                </div>
-                <div className={styles.serLink}>Читати більше &#8594;</div>
-              </div>
-            </Link>
-            <Link href='/services#financeId'>
-              <div className={styles.service}>
-                <div className={styles.serHeader}>Фінанси</div>
-                <div className={styles.serBody}>
-                  Побудова фінансової системи, облік фінансів, звіти P&L, Cash
-                  Flow, Balance...
-                </div>
-                <div className={styles.serLink}>Читати більше &#8594;</div>
-              </div>
-            </Link>
-            <Link href='/services#grantId'>
-              <div className={styles.service}>
-                <div className={styles.serHeader}>Гранти</div>
-                <div className={styles.serBody}>
-                  Формування бізнес-плану для подання на грант, грантовий
-                  супровід...
-                </div>
-                <div className={styles.serLink}>Читати більше &#8594;</div>
-              </div>
-            </Link>
-            <Link href='/services#kikId'>
-              <div className={styles.service}>
-                <div className={styles.serHeader}>КІК</div>
-                <div className={styles.serBody}>
-                  Що таке КІК? <br />
-                  КІК – Контрольована Іноземна Компанія...
-                </div>
-                <div className={styles.serLink}>Читати більше &#8594;</div>
-              </div>
-            </Link>
-            {/* <Link href='/services#yurId'>
-              <div className={styles.service}>
-                <div className={styles.serHeader}>Курси</div>
-                <div className={styles.serBody}>
-                  Юридичний супровід бізнесу, формування договорів...
-                </div>
-                <div className={styles.serLink}>Читати більше &#8594;</div>
-              </div>
-            </Link> */}
-            <Link href='/services#progId'>
-              <div className={styles.service}>
-                <div className={styles.serHeader}>Програмування BAS</div>
-                <div className={styles.serBody}>
-                  Програмування BAS Бухгалтерії на хмарному сервері...
-                </div>
-                <div className={styles.serLink}>Читати більше &#8594;</div>
-              </div>
-            </Link>
-            <Link href='/services#diaId'>
-              <div className={styles.service}>
-                <div className={styles.serHeader}>Дія.City</div>
-                <div className={styles.serBody}>
-                  Резиденти Дія.City що потрібно знати
-                  <br /> для початку роботи...
-                </div>
-                <div className={styles.serLink}>Читати більше &#8594;</div>
-              </div>
-            </Link>
-            <Link href='/services#yurId'>
-              <div className={styles.service}>
-                <div className={styles.serHeader}>Юридичні послуги</div>
-                <div className={styles.serBody}>
-                  Юридичний супровід бізнесу, формування договорів...
-                </div>
-                <div className={styles.serLink}>Читати більше &#8594;</div>
-              </div>
-            </Link>
+              </Link>
+            ))}
           </div>
         </div>
       </div>
-      <div className={styles.carusel + ' ' + 'mt100'}>
-        <div className='container'>
+      <div className={styles.services}>
+        <div className="container">
+          <div className="title mt100 center">Юридичні послуги</div>
+          <div className={styles.serviceWrapper}>
+            {yurserv?.map((buh, idx) => (
+              <Link key={idx} href={`/legalservices#${buh.slogId}`}>
+                <div className={styles.service}>
+                  <div className={styles.serHeader}>{buh.name}</div>
+                  <div className={styles.serBody}>{buh.description}</div>
+                  <div className={styles.serLink}>Читати більше &#8594;</div>
+                </div>
+              </Link>
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className={styles.services}>
+        <div className="container">
+          <div className="title mt100 center">Пакети послуг</div>
+          <div className={styles.serviceWrapper}>
+            {blockserv?.map((buh, idx) => (
+              <Link key={idx} href={`/servicepackeges#${buh.slogId}`}>
+                <div className={styles.service}>
+                  <div className={styles.serHeader}>{buh.name}</div>
+                  <div className={styles.serBody}>{buh.description}</div>
+                  <div className={styles.serLink}>Читати більше &#8594;</div>
+                </div>
+              </Link>
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className={styles.services}>
+        <div className="container">
+          <div className="title mt100 center">Фінансові послуги</div>
+          <div className={styles.serviceWrapper}>
+            {finserv?.map((buh, idx) => (
+              <Link key={idx} href={`/financeservices#${buh.slogId}`}>
+                <div className={styles.service}>
+                  <div className={styles.serHeader}>{buh.name}</div>
+                  <div className={styles.serBody}>{buh.description}</div>
+                  <div className={styles.serLink}>Читати більше &#8594;</div>
+                </div>
+              </Link>
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className={styles.carusel + " " + "mt100"}>
+        <div className="container">
           <Carusel />
         </div>
       </div>
       <div className={styles.message}>
-        <div className='container'>
+        <div className="container">
           <div className={styles.mesText}>
-            <div className='redText'>швидка дія</div>
-            <div className='title'>Замовити консультацію</div>
+            <div className="redText">швидка дія</div>
+            <div className="title">Замовити консультацію</div>
             <div className={styles.messegeWrapper}>
               <div className={styles.mesLeft}>
-                <div className='text mt20'>
+                <div className="text mt20">
                   Тут Ви можете залишити запит на консультацію. Ми зателефонуємо
                   Вам та проінформуємо про вартість і час проведення зустрічі. У
                   разі, якщо Ваше запитання можна вирішити в режимі переписки,
                   ми надамо Вам зворотній зв'язок найближчим часом .
                 </div>
               </div>
-              <div className={styles.sendMessage + ' mt20'}>
-                <div className='formGroup'>
+              <div className={styles.sendMessage + " mt20"}>
+                <div className="formGroup">
                   <input
-                    type='text'
+                    type="text"
                     placeholder="І'мя"
                     onChange={(e) => setName(e.target.value)}
                     value={name}
                   />
                 </div>
-                <div className='formGroup'>
+                <div className="formGroup">
                   <input
-                    type='text'
-                    placeholder='Телефон'
+                    type="text"
+                    placeholder="Телефон"
                     onChange={(e) => setPhone(e.target.value)}
                     value={phone}
                   />
                 </div>
-                <div className='formGroup'>
+                <div className="formGroup">
                   <button className={styles.button} onClick={sendMsg}>
                     Замовити послугу
                   </button>
