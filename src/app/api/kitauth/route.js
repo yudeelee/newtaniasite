5c44ab77a0350f796dfeb5f6626688e589f68719
Your Name <you@example.com>
2025-05-22 16:19:13 +0300

    upload2

--- src/app/api/kitauth/route.js
+++ src/app/api/kitauth/route.js
@@ -6,13 +6,14 @@ export async function GET() {
     // Your application logic to authenticate the user
     // For example, you can check if the user is logged in or has the necessary permissions
     // If the user is not authenticated, you can return an error response
-    const newToken = Math.random().toString();
+    let date = new Date();
+    date = date.toString();
     const { token, expire, signature } = getUploadAuthParams({
         
         privateKey: process.env.PRIVATE_KEY , // Never expose this on client side
         publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY ,
         // expire: 30 * 60, // Optional, controls the expiry time of the token in seconds, maximum 1 hour in the future
-        token: newToken, // Optional, a unique token for request
+        token: date, // Optional, a unique token for request
     })
 
     return Response.json({ token, expire, signature, publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY })
